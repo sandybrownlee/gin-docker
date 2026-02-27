261a142648d46c9f8ef34d815d4a5cade384746e
Dominik Sobania <dsobania@googlemail.com>
2026-02-27 10:20:18 +0100

    Add cloudlab_startup.sh for Docker installation

---
+++ cloudlab_startup.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+ 
+# Fix the users
+sudo groupadd docker
+newgrp docker
+sudo usermod -aG docker $(whoami)
+ 
+# Install docker
+sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y
+sudo apt install docker.io
+sudo systemctl stop docker
+sudo systemctl stop docker.socket
+sudo systemctl stop containerd
+sudo systemctl start docker
+ 
+docker --version
+groups $(whoami)
+sudo chmod 666 /var/run/docker.sock
+docker run hello-world
+ 
+echo ">> To build image run: docker build -t dockerfuzzer ."
